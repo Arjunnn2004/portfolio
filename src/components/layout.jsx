57fc8abb2a2ed9c9d0a7382d762cbf360d662594
Arjunnn2004 <arjunchawla7682@gmail.com>
2025-06-08 22:53:43 +0530

    bugsfixed

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import Header from './header';
 import Footer from './footer';
 import { Outlet } from 'react-router';
+import bg from './bg.jpg'; 
 
 function Layout() {
   const [currentTheme, setCurrentTheme] = useState('light');
 
   return (
-    <div className={`flex flex-col min-h-screen ${currentTheme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
+    <div className={`flex flex-col min-h-screen backdrop-blur-lg ${currentTheme === 'dark' ? 'bg-gray-900 text-black' : 'bg-white text-black'}` } style={{ backgroundImage: `url(${bg})` }}>
       <Header currentTheme={currentTheme} setCurrentTheme={setCurrentTheme} />
       <main className="flex-grow container mx-auto px-4 py-8">
         <Outlet />
