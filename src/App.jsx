784adedf0928bbbcfb7ef754373e763b24247072
Arjunnn2004 <arjunchawla7682@gmail.com>
2025-06-09 14:40:15 +0530

    bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import Card from "./components/Card";
 import "./App.css";
 import Button from "./components/Button";
 
-
 function App() {
-  const values = ['Web Developer', 'UI/UX Designer', 'Creative Coder'];
+  const values = ["Web Developer", "UI/UX Designer", "Creative Coder"];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
@@ -19,28 +18,39 @@ function App() {
     <div className="flex flex-col lg:justify-between gap-10 md:flex-col lg:flex-row ">
       <div className="flex flex-col flex-wrap">
         <br />
-        <h1 className="text-7xl font-extrabold ">Arjun Chawla</h1>
+        <h1 className="text-7xl font-extrabold transition-all duration-500">Arjun Chawla</h1>
 
         <p className="text-lg italic font-sans text-gray-600 transition-all duration-500">
-         <br />
-          I’m a <span style={{ cursor: "pointer", textDecoration: "underline" }}>{values[currentIndex]}</span> <br />
-          Welcome to my digital space! I specialize in building modern, responsive, and user-friendly web experiences. Whether it's crafting intuitive UI/UX designs, building full-stack applications, or solving complex coding problems — I'm always up for the challenge.
-<br />
-💡 Passionate about technology.
-<br />
-🔧 Obsessed with clean code.
-<br />
-🚀 Always learning, always growing.
-<br />
-Explore my projects, learn about my journey, and let’s connect to build something awesome together.
+          <br />
+          I’m a{" "}
+          <span style={{ cursor: "pointer", textDecoration: "underline" }}>
+            {values[currentIndex]}
+          </span>{" "}
+          <br />
+          Welcome to my digital space! I specialize in building modern,
+          responsive, and user-friendly web experiences. Whether it's crafting
+          intuitive UI/UX designs, building full-stack applications, or solving
+          complex coding problems — I'm always up for the challenge.
+          <br />
+          💡 Passionate about technology.
+          <br />
+          🔧 Obsessed with clean code.
+          <br />
+          🚀 Always learning, always growing.
+          <br />
+          Explore my projects, learn about my journey, and let’s connect to
+          build something awesome together.
         </p>
         <div className="mt-10 flex flex-row gap-4">
-        <Button value={`Projects`} link="https://github.com/arjunnn2004"/>
-        <Button value={`My Resume`} link="https://drive.google.com/file/d/1dQ_LKtkygSlnDbTCWF6QXlHKuMhgGEp7/view?usp=sharing"/>
+          <Button value={`Projects`} link="https://github.com/arjunnn2004" />
+          <Button
+            value={`My Resume`}
+            link="https://drive.google.com/file/d/1dQ_LKtkygSlnDbTCWF6QXlHKuMhgGEp7/view?usp=sharing"
+          />
         </div>
       </div>
       <div>
-      <Card/>
+        <Card />
       </div>
     </div>
   );
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,13 +2,17 @@ import React, { useState } from 'react';
 import Header from './header';
 import Footer from './footer';
 import { Outlet } from 'react-router';
-import bg from './bg.jpg'; 
+import dark from './dark.jpg'; 
+import bg from './bg.jpg'
 
 function Layout() {
   const [currentTheme, setCurrentTheme] = useState('light');
 
   return (
-    <div className={`flex flex-col min-h-screen backdrop-blur-lg ${currentTheme === 'dark' ? 'bg-gray-900 text-black' : 'bg-white text-black'}` } style={{ backgroundImage: `url(${bg})` }}>
+    <div
+      className={`flex flex-col min-h-screen backdrop-blur-lg ${currentTheme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-black'} transition-all duration-500`}
+      style={currentTheme === 'dark'?{ backgroundImage: `url(${dark})`, backgroundSize: 'cover' }:{ backgroundImage: `url(${bg})`, backgroundSize: 'cover' }}
+    >
       <Header currentTheme={currentTheme} setCurrentTheme={setCurrentTheme} />
       <main className="flex-grow container mx-auto px-4 py-8">
         <Outlet />
